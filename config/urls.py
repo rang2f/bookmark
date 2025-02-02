61c4c61108ab0343579b708cde888d177a254e9c
rang2f <banquo1605@gmail.com>
2025-02-02 15:15:16 +0900

    add list in main page

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('bookmark.urls')),
+    path('', include('list.urls')),
     path('bookmark/', include('bookmark.urls')),
     path('admin/', admin.site.urls),
 ]
